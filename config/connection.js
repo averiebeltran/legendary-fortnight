83b51183dafe2e27d5e7a3f747d53b28b1ce38f9
AverieBeltran <averiebeltran@github.com>
2021-05-24 22:56:24 -0500

    trying to fix heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
+    ? new Sequelize(process.env.JAWSDB_URL, {
+        dialect:  'postgres',
+        protocol: 'postgres',
+        logging:  true
+    })
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
